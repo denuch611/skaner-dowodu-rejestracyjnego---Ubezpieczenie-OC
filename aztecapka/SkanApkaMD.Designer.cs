975da6536277361fa589142d660e4293e0ed6b93
denuch611 <mdenuszek52@gmail.com>
2022-06-05 19:03:06 +0200

    Add project files.

---
+++ aztecapka/SkanApkaMD.Designer.cs
@@ -0,0 +1,337 @@
+﻿
+namespace aztecapka1
+{
+    partial class SkanApkaMD
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+          #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textID = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Skan wartość ID";
+            // 
+            // textID
+            // 
+            this.textID.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textID.Location = new System.Drawing.Point(133, 32);
+            this.textID.Name = "textID";
+            this.textID.Size = new System.Drawing.Size(622, 36);
+            this.textID.TabIndex = 1;
+            this.textID.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textID.Validating += new System.ComponentModel.CancelEventHandler(this.textID_Validating);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(593, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Wyczyść";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(47, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Marka Pojazdu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(252, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "VIN";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(27, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 19);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Numer Rejestracyjny";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(185, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(210, 19);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Data Zarejestrowania pojazdu";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(593, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 58);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Pobierz Dane";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(213, 276);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 45);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Pierwsza Data zarejestrowania pojazdu";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(0, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 23);
+            this.label9.TabIndex = 31;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(411, 128);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(129, 19);
+            this.label10.TabIndex = 27;
+            this.label10.Text = "Nazwa właściciela";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(401, 205);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(148, 19);
+            this.label11.TabIndex = 29;
+            this.label11.Text = "Nazwisko właściciela";
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(593, 256);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 58);
+            this.button3.TabIndex = 30;
+            this.button3.Text = "Zamknij";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(47, 205);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 19);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "Model Pojazdu";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(201, 148);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 22);
+            this.textBox1.TabIndex = 33;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(201, 316);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 22);
+            this.textBox2.TabIndex = 34;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(26, 316);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(149, 22);
+            this.textBox3.TabIndex = 35;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.Location = new System.Drawing.Point(201, 227);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(149, 22);
+            this.textBox4.TabIndex = 36;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox5.Location = new System.Drawing.Point(26, 227);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(149, 22);
+            this.textBox5.TabIndex = 37;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox6.Location = new System.Drawing.Point(26, 148);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(149, 22);
+            this.textBox6.TabIndex = 38;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox7.Location = new System.Drawing.Point(401, 227);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(149, 22);
+            this.textBox7.TabIndex = 39;
+            // 
+            // textBox8
+            // 
+            this.textBox8.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.textBox8.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox8.Location = new System.Drawing.Point(401, 148);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox8.Size = new System.Drawing.Size(149, 22);
+            this.textBox8.TabIndex = 40;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(441, 316);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 41;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // SkanApkaMD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textID);
+            this.Controls.Add(this.label1);
+            this.Name = "SkanApkaMD";
+            this.Text = "SkanApkaMD";
+            this.Load += new System.EventHandler(this.SkanApkaMD_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textID;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ aztecapka/SkanApkaMD.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Reflection;
+using System.Threading;
+using OpenQA.Selenium.Safari;
+using System.Web;
+using System.Globalization;
+
+namespace aztecapka1
+{
+    public partial class SkanApkaMD : Form
+    {
+        public SkanApkaMD()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Action<Control.ControlCollection> func = null;
+            func = (controls) =>
+            {
+                foreach (Control control in controls)
+                {
+                    if (control is TextBox)
+                    {
+                        (control as TextBox).Clear();
+                    }
+                    else
+
+                    {
+                        func(control.Controls);
+                    }
+                }
+
+            };
+            func(Controls);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string idpojazdu = textID.Text;
+            var decoder = new PolishVehicleRegistrationCertificateDecoder.Decoder();
+            var vehicleRegistrationInfo = decoder.Decode(idpojazdu);
+
+            if (string.IsNullOrEmpty(textBox6.Text))
+            {
+            textBox6.AppendText(vehicleRegistrationInfo.MarkaPojazdu);
+            textBox1.AppendText(vehicleRegistrationInfo.VinNrNadwozia);
+            textBox3.AppendText(vehicleRegistrationInfo.NrRejestracyjny);
+            textBox4.AppendText(vehicleRegistrationInfo.DataWydaniaAktualnegoDr);
+            textBox2.AppendText(vehicleRegistrationInfo.DataPierwszejRejestracji);
+            textBox7.AppendText(vehicleRegistrationInfo.NazwiskoWlascicielaPojazdu);
+            textBox8.AppendText(vehicleRegistrationInfo.NazwaWlascicielaPojazdu);
+            textBox5.AppendText(vehicleRegistrationInfo.ModelPojazdu);
+            }
+            string message = "Dodano dane";
+            DialogResult result;
+
+            result = MessageBox.Show(message);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                this.Close();
+            }
+
+            else
+            {
+                textBox6.Clear();
+                textBox1.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox2.Clear();
+                textBox7.Clear();
+                textBox8.Clear();
+                textBox5.Clear();
+
+                textBox6.AppendText(vehicleRegistrationInfo.MarkaPojazdu);
+                textBox1.AppendText(vehicleRegistrationInfo.VinNrNadwozia);
+                textBox3.AppendText(vehicleRegistrationInfo.NrRejestracyjny);
+                textBox4.AppendText(vehicleRegistrationInfo.DataWydaniaAktualnegoDr);
+                textBox2.AppendText(vehicleRegistrationInfo.DataPierwszejRejestracji);
+                textBox7.AppendText(vehicleRegistrationInfo.NazwiskoWlascicielaPojazdu);
+                textBox8.AppendText(vehicleRegistrationInfo.NazwaWlascicielaPojazdu);
+                textBox5.AppendText(vehicleRegistrationInfo.ModelPojazdu);
+
+            }
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless");
+
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+
+
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://historiapojazdu.gov.pl/");
+
+            var NRREJESTR = driver.FindElement(By.Id("_historiapojazduportlet_WAR_historiapojazduportlet_:rej"));
+            var VIN = driver.FindElement(By.Id("_historiapojazduportlet_WAR_historiapojazduportlet_:vin"));
+            var data = driver.FindElement(By.Id("_historiapojazduportlet_WAR_historiapojazduportlet_:data"));
+            var loginButton = driver.FindElement(By.XPath("//input[@value='Sprawdź pojazd »']"));
+
+
+            NRREJESTR.SendKeys(vehicleRegistrationInfo.NrRejestracyjny);
+            VIN.SendKeys(vehicleRegistrationInfo.VinNrNadwozia);
+            var date = Convert.ToDateTime(vehicleRegistrationInfo.DataPierwszejRejestracji);
+         string dataToTrim= date.ToString("dd.MM.yyyy");
+            data.Click();
+            data.SendKeys((dataToTrim));
+            loginButton.Click();
+
+            var titles = driver.FindElements(By.ClassName("oc"));
+
+
+
+            foreach (var title in titles)
+
+
+                Console.WriteLine(title.Text);
+
+            var oc = driver.FindElement(By.ClassName("oc")).GetAttribute("innerText");
+            Console.WriteLine(oc);
+
+            var badanieTech = driver.FindElement(By.ClassName("tech")).GetAttribute("innerText");
+            Console.WriteLine(badanieTech);
+
+
+            var osczasu = driver.FindElement(By.Id("raport-content-template-timeline-button"));
+
+            osczasu.Click();
+
+            var test1 = driver.FindElement(By.ClassName("summary-box")).GetAttribute("innerText");
+            Console.WriteLine(test1);
+
+
+            var test2 = driver.FindElement(By.XPath("//p[contains(text(), 'Polisa OC: ')]/span")).GetAttribute("innerText");
+            Console.WriteLine(test2);
+
+            //div[text()='Ingredients:']/following-sibling::div/span[contains(@class,'cancel-icon')]
+
+            //div[@class='ingredients-container-header-close']/span[@class='material-icons cancel-icon ']
+            //("//div[@class='item-inner']/span[@class='title']"));
+            //Console.ReadKey();
+            //*[@id="timeline-summary-box"]/div[2]/p[6]
+
+
+
+
+            Thread.Sleep(3000);
+            driver.Quit();
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+        ErrorProvider errorProvider = new ErrorProvider();
+        private void textID_Validating(object sender, CancelEventArgs e)
+
+        {
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void SkanApkaMD_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=NOTEBOOK-MD\MSSQLSERVER;Initial Catalog=test;Integrated Security=True;");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("insert into test100(nameid, id, imie) values('" + 1 + "','" + 1 + "','" + textBox1.Text + "')", conn);
+            int i = cmd.ExecuteNonQuery();
+
+            if (i != 0)
+            {
+                MessageBox.Show("poszlo");
+
+            }
+            else
+            {
+                MessageBox.Show("lipa");
+            }
+        }
+    }
+
+}
+
